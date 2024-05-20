655cb9e8b42694857ecf8a6fc5c3dcc08476a566
joshcpld <joshcpld@gmail.com>
2024-05-20 16:35:31 +1000

    Submitted ECOM90024 assignment 2

--- ECON90010 - QAFII/qafii_assignment_dataset.R
+++ ECON90010 - QAFII/qafii_assignment_dataset.R
@@ -1,24 +1,51 @@
 library(tidyverse)
+library(readxl)
 library(lubridate)
-
+library(janitor)
+        
+        
 setwd("C:/Users/joshc/OneDrive/Desktop/git/mae_unimelb/ECON90010 - QAFII/")
 
-country_stocks <- read_csv("country_stocks.csv") %>% 
-  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
-  
+SP500 <- read_excel("SP500.xlsx") %>% 
+  clean_names() %>%  
+  select(Date = date, SP500 = last_price)
+
+DAX <- read_excel("DAX.xlsx")
+
+DJI <- read_excel("DJI.xlsx")
+
+FTSE <- read_excel("FTSE.xlsx")
+
+HSI <- read_excel("HSI.xlsx")
+
+NKI <- read_excel("NKI.xlsx")
+
+MXWO <- read_excel("MXWO.xlsx")
+
+VIX <- read_excel("VIX.xlsx")
 
 TBILL <- read_csv("TBILL.csv") %>% 
-  # For some reason there are a bunch of missing values. 
-  #Some are US public holidays (Thanksgiving/ 4th July). But not sure what many others are.
-  mutate_all(na_if, ".") %>% 
-  mutate(Date = as.Date(Date, format = "%d/%m/%Y"))
+  select(Date = DATE, TBILL = DTB3) %>% 
+  mutate_at(vars(TBILL), ~na_if(., "."))
 
 
-country_stocks_augmented <- country_stocks %>% 
-  full_join(MXWO, by = "Date") %>% 
-  full_join(TBILL, by = "Date")
+data <- MXWO %>% 
+  full_join(DAX, by = "Date") %>% 
+  full_join(DJI, by = "Date") %>%
+  full_join(FTSE, by = "Date") %>%
+  full_join(HSI, by = "Date") %>%
+  full_join(NKI, by = "Date") %>%
+  full_join(SP500, by = "Date") %>%
+  full_join(VIX, by = "Date") %>% 
+  full_join(TBILL, by = "Date") %>% 
+  select(Date, DJI, SP500, HSI, NKI, DAX, FTSE, MXWO, TBILL, VIX) %>% 
+  filter(Date >= as.Date("1990-01-01")) %>% 
+  mutate(Date = as.Date(Date))
+
+data_no_nan <- data %>% 
+  na.omit()
 
 
-write_csv(country_stocks_augmented, "country_stocks_augmented.csv")
+write_csv(data, "country_stocks.csv")
 
-
+write_csv(data_no_nan, "country_stocks_no_nan.csv")
